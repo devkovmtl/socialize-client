0e43f5eb77d0d07f537b23fe74861ff8f8b2622b
Mikael <dev.kov.mtl@gmail.com>
2022-04-07 14:56:12 -0400

    Basic setup of react-router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-const App = () => {
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Feed from './pages/Feed';
+import Profile from './pages/Profile';
+
+const Layout = () => {
   return (
     <div>
-      <h1 className='text-blue-400 text-3xl font-bold underline'>
-        Hello World
-      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Social Club</Link>
+        </li>
+        <li>
+          <Link to='profile'>Profile</Link>
+        </li>
+        <li>
+          <Link to='login'>Login</Link>
+        </li>
+        <li>
+          <Link to='register'>Register</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Feed />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Route>
+    </Routes>
   );
 };
 
