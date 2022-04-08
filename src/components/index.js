242d569b7cb4ac3cdfd95a9d4f4d476a150cd36c
Mikael <dev.kov.mtl@gmail.com>
2022-04-08 18:08:15 -0400

    export component from a single file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Spinner } from './Spinner';
+export { default as PageTitle } from './PageTitle';
+export { default as PageDescription } from './PageDescription';
+export { default as LabelForm } from './LabelForm';
+export { default as InputField } from './InputField';
+export { default as InputFieldError } from './InputFieldError';
