b6fb167881f163b029bd165da355f05a02f1e265
Mikael <dev.kov.mtl@gmail.com>
2022-04-13 16:44:35 -0400

    updated layout with navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,30 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { Outlet, Link, useNavigate } from 'react-router-dom';
-import { logout } from '../redux/authSlice';
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const Layout = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-
-  const handleLogout = () => {
-    dispatch(logout());
-    navigate('/login');
-  };
 
   return (
     <div>
-      {isLoggedIn && (
-        <ul>
-          <li>
-            <Link to='/'>Social Club</Link>
-          </li>
-          <li>
-            <Link to='profile'>Profile</Link>
-          </li>
-          <button onClick={handleLogout}>Logout</button>
-        </ul>
-      )}
+      {isLoggedIn && <Navbar />}
       <Outlet />
     </div>
   );
