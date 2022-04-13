cc00b38c1fd21c8d971156a9138dc13e73087f0a
Mikael <dev.kov.mtl@gmail.com>
2022-04-13 16:45:30 -0400

    Added interceptors to check for token in response

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 // Protect the routes with the RequireAuth component
 import { RequireAuth, IsLoggedIn } from './helpers';
+import { logout } from './redux/authSlice';
 // Pages
 import Layout from './pages/Layout';
 import Login from './pages/Login';
@@ -8,8 +10,31 @@ import Register from './pages/Register';
 import Feed from './pages/Feed';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
+import axios from 'axios';
+import { ACCESS_TOKEN_KEY } from './constants';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // intercept 401 403 response  and redirect to login
+  axios.interceptors.response.use(
+    (response) => {
+      // check if the response is a success and if theris is a token
+      if (response.success && response.accessToken) {
+        // set the token in the local storage
+        localStorage.setItem(ACCESS_TOKEN_KEY, response.accessToken);
+      }
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401 || error.response.status === 403) {
+        dispatch(logout());
+        navigate('/login');
+      }
+    }
+  );
+
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
