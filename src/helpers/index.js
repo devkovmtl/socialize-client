71f352cdc618973884ec4934d7f24d0f600f637c
Mikael <dev.kov.mtl@gmail.com>
2022-04-15 14:33:30 -0400

    update toast notification

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,4 @@ export { default as getJwtExpiresIn } from './getJwtExpiresIn';
 export { default as checkIsTokenExpires } from './checkIsTokenExpires';
 export { default as RequireAuth } from './RequireAuth';
 export { default as IsLoggedIn } from './IsLoggedIn';
+export * from './toastNotification';
