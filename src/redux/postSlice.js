ff781ee82828fbc0e0e87fbf006730bac63418bd
Mikael <dev.kov.mtl@gmail.com>
2022-04-16 18:08:49 -0400

    fetch post from slice reducers

--- src/redux/postSlice.js
+++ src/redux/postSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { POSTS_URL } from '../constants';
+
+export const fetchPosts = createAsyncThunk(
+  'posts/fetchPosts',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(POSTS_URL);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue('Error fetching posts');
+    }
+  }
+);
 
 export const postSlice = createSlice({
   name: 'post',
@@ -8,6 +22,23 @@ export const postSlice = createSlice({
     error: null,
   },
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'pending';
+        state.posts = [];
+        state.error = null;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'fulfilled';
+        state.posts = state.posts.concat(action.payload.posts);
+        state.error = null;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.payload;
+      });
+  },
 });
 
 export default postSlice.reducer;
